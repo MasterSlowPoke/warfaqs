015dfd7f94e7b9a7ed59e449d7959c72672d1b60
Craig Sniffen <craigsniffen@craigsniffen.com>
2015-01-05 10:15:22 -0500

    added basic ajax retrival of data slates

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,6 +2,30 @@ $(function() {
   $(document).on('click', '.source', function(e) {
     e.preventDefault();
     $.ajax({
+      url: "/dataslates.json",
+      data: {
+        source_id: 1
+      },
+      type: "GET",
+      dataType : "json",
+ 
+      success: function( json ) {
+        console.log(json);
+        console.log("fads");
+        $(json).each(function(i, dataslate) {
+          console.log(dataslate['title']);
+        });
+      },
+ 
+      error: function( xhr, status, errorThrown ) {
+        alert( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+      },
+ 
+      complete: function( xhr, status ) {
+      },
     });
   });
 });
