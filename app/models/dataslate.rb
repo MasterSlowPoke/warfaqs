5fb6a027552abc49c6e89158a3402ac6dd995d5b
Craig Sniffen <craigsniffen@craigsniffen.com>
2015-01-05 10:16:45 -0500

    added scope for selecting by role name

--- app/models/dataslate.rb
+++ app/models/dataslate.rb
@@ -1,6 +1,9 @@
 class Dataslate < ActiveRecord::Base
   has_many :dataslate_sources
   has_many :sources, through: :dataslate_sources
+
+  scope :role, ->(role_name) { where(role: role_name) }
+
   def most_recent_source
     sources.order(published: :desc).limit(1)
   end
