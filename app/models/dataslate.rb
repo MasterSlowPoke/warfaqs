b16708790d0962631972c810127bc043795f2442
Craig Sniffen <craigsniffen@craigsniffen.com>
2015-01-05 10:16:28 -0500

    added method to retrieve most recent source

--- app/models/dataslate.rb
+++ app/models/dataslate.rb
@@ -1,4 +1,7 @@
 class Dataslate < ActiveRecord::Base
   has_many :dataslate_sources
   has_many :sources, through: :dataslate_sources
+  def most_recent_source
+    sources.order(published: :desc).limit(1)
+  end
 end
