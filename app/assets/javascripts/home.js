1a3d2ba615351a7e448de22d488eb7ef6980142d
Craig Sniffen <craigsniffen@craigsniffen.com>
2015-01-06 09:24:48 -0500

    put the data slate info into #dataslates

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,9 +10,26 @@ $(function() {
       dataType : "json",
  
       success: function( json ) {
+        list = $('#dataslates ul');
+        list.html('');
         $(json).each(function(i, dataslate) {
-          console.log(dataslate['title']);
+          list.append("<li><a href='#' class='dataslate' data-dataslate-id='" + dataslate['id'] + "'>" + dataslate['title'] + '</a></li>');
         });
+        $('#dataslates').removeClass('hidden');  
+      },
+ 
+      error: function( xhr, status, errorThrown ) {
+        alert( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+      },
+ 
+      complete: function( xhr, status ) {
+      },
+    });
+  });
+
       },
  
       error: function( xhr, status, errorThrown ) {
