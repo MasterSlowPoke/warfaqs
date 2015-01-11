5b2c01e4a4403dba6c7df08bf5c273567e46af74
Craig Sniffen <craigsniffen@craigsniffen.com>
2015-01-11 01:17:10 -0500

    created picturable concern

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -1,7 +1,6 @@
 class Faction < ActiveRecord::Base
+  include Picturable
+
   has_many :dataslate_factions
   has_many :dataslates, through: :dataslate_factions
-
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
